bf3da3d2b6ec6912e71a97f1a107520ea78ce0b3
limbera <limbera@gmail.com>
2025-05-13 20:02:43 -0700

    v1.2.1 bump

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ module.exports = {
     expo: {
       name: "Dripmax",
       slug: "dripmax-app",
-      version: "1.2.0",
+      version: "1.2.1",
       orientation: "portrait",
       icon: "./assets/images/icon.png",
       scheme: "dripmax",
